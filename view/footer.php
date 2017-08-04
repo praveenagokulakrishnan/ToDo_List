8ce37f4cebe0ceef6e69bda6f4e1f76adf2bf4e3
praveena gokulakrishnan <pg376@afsconnect1.njit.edu>
2017-08-04 17:05:57 -0400

    Including header and footer files

---
+++ view/footer.php
@@ -0,0 +1,8 @@
+<!-- Footer layout for the entire application -->
+<footer>
+    <p class="copyright">
+        &copy; <?php echo date("Y"); ?> Todo List, Inc.
+    </p>
+</footer>
+</body>
+</html>
---
+++ view/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<!-- Header layout for the entire application -->
+<html>
+<!-- the head section -->
+<head>
+    <title>Todo List - Portal</title>
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<!-- the body section -->
+<body>
+<header>
+    <h1>
+        Todo List - Portal
+    </h1>
+</header>
